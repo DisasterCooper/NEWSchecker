832057782f090fb1cb8a0b76f8f24b4d24f5c3fa
DisasterCooper <bajdukmax@gmail.com>
2023-09-17 20:58:58 +0300

    Update abstract class, add class NewsType

--- parser/joining_parser.py
+++ parser/joining_parser.py
@@ -1,9 +1,17 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
-
+from typing import TypedDict
 from bs4 import Tag
 
-from news.models import NewsSource
+
+class NewsType(TypedDict):
+    """
+    Helps to understand the type of news dictionary.
+    """
+    title: str
+    link: str
+    content: str
+    published: datetime
 
 
 class JoiningParser(ABC):
@@ -12,18 +20,18 @@ class JoiningParser(ABC):
     """
 
     def __init__(self):
-        self.source = self._create_source()
         self.title = self._get_title()
         self.content = self._get_content()
-        self.link = self._get_link()
 
     @abstractmethod
-    def get_last_news(self) -> list[dict]:
-        """Returns last news from chosen source."""
+    def get_last_news(self, from_datetime: datetime) -> list[NewsType]:
+        """
+        Returns last news from chosen source.
+        """
         raise NotImplementedError("This method should be implemented in subclasses")
 
     @abstractmethod
-    def _create_source(self) -> NewsSource:
+    def get_web_link(self) -> str:
         pass
 
     @abstractmethod
@@ -32,10 +40,6 @@ class JoiningParser(ABC):
 
     @abstractmethod
     def _get_content(self, item: Tag) -> str:
-        pass
-
-    @abstractmethod
-    def _get_link(self, item: Tag) -> str:
         pass
 
     @abstractmethod
