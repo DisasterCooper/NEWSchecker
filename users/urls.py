20f1a0268273801053e83ca69d7ec83856572f66
DisasterCooper <bajdukmax@gmail.com>
2023-10-03 02:11:10 +0300

    Refactoring registration and login

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from . import views
+from users.views import Login, LogOut
 
 #  /users/
 
 app_name = "users"
 
 urlpatterns = [
-    path('<int:user_id>/', views.RedactUser.as_view(), name='redact_user'),
+    path("<int:users_id>/", views.RedactUser.as_view(), name="redact_user"),
+    path("login/", Login, name="login"),
+    path("logout/", LogOut, name="logout"),
         ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
+from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 from django.views import View, generic
+from flower.views.auth import authenticate
 
 from .forms import UserRegisterForm, UserRedactForm
 from .models import User
@@ -8,24 +11,45 @@ from .models import User
 class Registration(View):
 
     def get(self, request):
-        return render(request, "users/registration.html", {"form": UserRegisterForm()})
+        return render(request, "registration/registration.html", {"form": UserRegisterForm()})
 
     def post(self, request):
 
         form = UserRegisterForm(request.POST)
 
         if not form.is_valid():
-            return render(request, "users/registration.html", {"form": form})
+            return render(request, "registration/registration.html", {"form": form})
 
         user = form.save(commit=False)  # Создаем объект, без сохранения в БД
         user.is_active = False
         user.save()
 
-        return redirect("")
+        return redirect("accounts:login")
 
 
 class RedactUser(generic.UpdateView):
     model = User
     form_class = UserRedactForm
-    pk_url_kwarg = "user_id"
+    pk_url_kwarg = "users_id"
     template_name = "users/redact_user.html"
+
+
+def Login(request):
+    if request.user.is_authenticated:
+        return redirect("news:home")
+    else:
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("news:home")
+            else:
+                messages.error(request, "Username or password is incorrect")
+        return render(request, "registration/login.html")
+
+
+def LogOut(request):
+    logout(request)
+    return redirect("accounts:login")
